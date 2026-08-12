63890629dff0e01652a09c47384e47ba1ed5653d
Kalpan2007 <kalpankaneriya@gmail.com>
2026-08-12 11:27:33 +0530

    Update next.config.ts with Render backend image domain

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -11,7 +11,7 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: 'https',
-        hostname: '*.onrender.com',
+        hostname: 'djangobnb-backend-0bj3.onrender.com',
       },
     ],
   },
